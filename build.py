7121dc733fff0b7227c599a0754081dbd3da6640
Liang Hai <lianghai@gmail.com>
2017-01-31 00:44:18 +0800

    Start updating to HindKit

--- build.py
+++ build.py
@@ -1,165 +1,53 @@
-#!/usr/bin/python
+#! /usr/bin/env AFDKOPython
+# encoding: UTF-8
+from __future__ import division, absolute_import, print_function, unicode_literals
+import hindkit as kit
 
-import argparse, sys, subprocess, os.path
-import WriteFeaturesMarkFDK
+family = kit.Family(
+    trademark = "Halant",
+    script_name = "Devanagari",
+    client_name = "Google Fonts",
+    initial_release_year = 2014,
+)
+family.set_masters()
+family.set_styles([
+    ("Light",     0, 300),
+    ("Regular",  21, 400),
+    ("Medium",   44, 500),
+    ("SemiBold", 70, 600),
+    ("Bold",    100, 700),
+])
 
-import itf
-from config import (
-    FAMILY_NAME, STYLE_NAMES, UFOIG_ARGS,
-    MATCH_mI_OFFSETS_DICT, MAKEOTF_ARGS, OUTPUT_DIR
+i = family.info
+i.openTypeNameDesigner = "Vivek Sadamate and Ninad Kale (Devanagari), Jonny Pinhorn (Latin)"
+i.openTypeHheaAscender, i.openTypeHheaDescender, i.openTypeHheaLineGap = 1050, -350, 100
+i.openTypeOS2TypoAscender, i.openTypeOS2TypoDescender, i.openTypeOS2TypoLineGap = 1050, -350, 100
+i.openTypeOS2WinAscent, i.openTypeOS2WinDescent = 1100, 400
+
+project = kit.Project(
+    family,
+    fontrevision = "2.100",
+    options = {
+
+        "prepare_kerning": True,
+        "prepare_mark_positioning": True,
+
+        "match_mI_variants": "single",
+        "position_marks_for_mI_variants": True,
+
+        "build_ttf": True,
+        "do_style_linking": True,
+
+        # "use_os_2_version_4": True,
+        #     "prefer_typo_metrics": True,
+        #     "is_width_weight_slope_only": True,
+
+        "additional_unicode_range_bits": [0, 1, 2],
+
+    },
 )
 
+# (light_min, light_max), (bold_min, bold_max)
+# project.adjustment_for_matching_mI_variants = (0, 0), (0, 0)
 
-parser = argparse.ArgumentParser()
-
-procedures = parser.add_argument_group(
-    title='build procedure triggers',
-    description='execute `python build.py -grimc` to run all the procedures.'
-)
-
-procedures.add_argument(
-    '-g', '--generate', action='store_true',
-    help='generate OpenType classes'
-)
-procedures.add_argument(
-    '-r', '--reset', action='store_true',
-    help='reset style/instance directories'
-)
-procedures.add_argument(
-    '-i', '--instance', action='store_true',
-    help='generate instances'
-)
-procedures.add_argument(
-    '-m', '--match', action='store_true',
-    help='match mI (i matra) variants to base glyphs'
-)
-procedures.add_argument(
-    '-c', '--compile', action='store_true',
-    help='compile OTFs'
-)
-procedures.add_argument(
-    '--nointerpolate', action='store_true',
-    help='do not interpolate the masters'
-)
-
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(2)
-
-args = parser.parse_args()
-
-
-if args.generate:
-    itf.generate_classes(directory = 'masters', suffix = '_0')
-
-
-if args.reset:
-
-    print '\n#ITF: Resetting style/instance directories...'
-
-    subprocess.call(['rm', '-fr', 'styles'])
-    subprocess.call(['mkdir', 'styles'])
-
-    for style_name in STYLE_NAMES:
-
-        print '\tResetting %s...' % style_name
-
-        style_dir = itf.STYLES_DIR + style_name
-        IsBoldStyle_value = 'false'
-
-        subprocess.call(['mkdir', style_dir])
-
-        with open(style_dir + '/features', 'w') as f:
-            f.write(itf.TEMPLATE_FEATURES)
-
-        with open(style_dir + '/fontinfo', 'w') as f:
-            if style_name == 'Bold':
-                IsBoldStyle_value = 'true'
-            f.write(itf.TEMPLATE_FONTINFO % IsBoldStyle_value)
-
-    print '#ITF: Done.\n'
-
-
-if args.instance:
-
-    masters = [
-        i for i in [
-            itf.get_font('masters', suffix) for suffix in ['_0', '_1']
-        ] if i
-    ]
-
-    if args.nointerpolate:
-
-        for font, style_name in zip(masters, STYLE_NAMES):
-
-            print "\n#ITF: %s" % style_name
-
-            style_dir = 'styles/' + style_name
-
-            subprocess.call([
-                'cp', '-fr', font.path,
-                style_dir + '/font.ufo'
-            ])
-
-            if '-mark' in UFOIG_ARGS:
-                WriteFeaturesMarkFDK.MarkDataClass(
-                    font = itf.get_font(style_dir),
-                    folderPath = style_dir,
-                    trimCasingTags = False,
-                    genMkmkFeature = True if '-mkmk' in UFOIG_ARGS else False,
-                    writeClassesFile = True if '-clas' in UFOIG_ARGS else False,
-                    indianScriptsFormat = True if '-indi' in UFOIG_ARGS else False
-                )
-
-            if '-flat' in UFOIG_ARGS:
-                print "#ITF: Flattening the glyphs..."
-                subprocess.Popen(
-                    ['checkoutlines', '-e', style_dir + '/font.ufo'],
-                    stderr=subprocess.STDOUT,
-                    stdout=subprocess.PIPE
-                ).communicate()
-                print "#ITF: Done."
-
-    else:
-
-        itf.fix_Glyphs_UFO_masters(masters)
-
-        subprocess.call(
-            ['UFOInstanceGenerator.py', 'masters', '-o', 'styles'] + UFOIG_ARGS
-        )
-
-
-if args.match:
-
-    print '\n#ITF: Matching mI...\n'
-
-    for style_name in STYLE_NAMES:
-        print '\t%s...' % style_name
-        itf.match_mI(style_name, MATCH_mI_OFFSETS_DICT[style_name])
-        print '\t%s done.\n' % style_name
-
-    print '#ITF: Done.\n'
-
-
-if args.compile:
-
-    subprocess.call(['rm', '-fr', 'build'])
-    subprocess.call(['mkdir', 'build'])
-
-    for style_name in STYLE_NAMES:
-
-        style_dir = 'styles/' + style_name
-        otf_path = 'build/%s-%s.otf' % (FAMILY_NAME, style_name)
-
-        subprocess.call([
-            'makeotf',
-            '-f', style_dir + '/font.ufo',
-            '-o', otf_path,
-            '-mf', 'FontMenuNameDB',
-            '-gf', 'GlyphOrderAndAliasDB',
-        ] + MAKEOTF_ARGS)
-
-        subprocess.call(['rm', '-f', style_dir + '/current.fpr'])
-
-        if os.path.exists(otf_path) and os.path.exists(OUTPUT_DIR):
-            subprocess.call(['cp', '-f', otf_path, OUTPUT_DIR])
+project.build()
